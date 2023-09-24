f73f6de4cf2387b27b4222bde19ed3df9506321b
Vikash Kumar <vikash0cv@gmail.com>
2023-09-24 18:13:47 +0530

    api routes in tasks.js

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -29,15 +29,45 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const { userId } = req.query;
-    let response = await TodoModel.find({ userId: userId });
+    let response = await Tasks.find({ userId: userId });
     if (response) {
       res.status(200).json({ data: response, status: 200 });
     } else {
-      res.json({ message: "No data found!" });
+      res.json({ message: "No tasks found!" });
     }
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Error while loading To-dos.", error: error });
+      .json({ message: "Error while loading tasks", error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    let response = await Tasks.find({ _id: id });
+    if (response) {
+      res.status(200).json({ data: response, status: 200 });
+    } else {
+      res.json({ message: "task not found!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error while loading task", error: error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    let response = await Tasks.findByIdAndDelete({ _id: id });
+    if (response) {
+      res.status(200).json({ message: "task deleted!", status: 200 });
+    } else {
+      res.json({ message: "task not found!" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error while deleting task", error: error });
   }
 });
